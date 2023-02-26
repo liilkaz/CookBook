56c8daa1a708a45ce2a8442fcb2eadd5c2a669a4
Liliya (Tacarlen) <l.feodotova296@yandex.ru>
2023-02-26 19:08:04 +0300

    Initial Commit

---
+++ СookBook/Controller/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  СookBook
+//
+//  Created by Лилия Феодотова on 26.02.2023.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
---
+++ СookBook/Controller/RecipeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RecipeViewController.swift
+//  СookBook
+//
+//  Created by Лилия Феодотова on 26.02.2023.
+//
+
+import UIKit
+
+class RecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+}
