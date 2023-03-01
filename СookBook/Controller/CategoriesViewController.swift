874fc3589e21e8ec59d741f2939b57114f126e04
Leonid <78638629+theheleo@users.noreply.github.com>
2023-03-01 19:15:05 +0300

    CategoriesModule закончена верстка

--- СookBook/Controller/CategoriesViewController.swift
+++ СookBook/Controller/CategoriesViewController.swift
@@ -3,6 +3,7 @@ import UIKit
 class CategoriesViewController: UIViewController {
     
     private let categoriesLabelAndImageView = CategoriesLabelAndImageView()
+    private let categoriesCollectionView = CategoriesCollectionView()
         
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,9 +15,8 @@ class CategoriesViewController: UIViewController {
     private func setupViews() {
         view.backgroundColor = .white
         view.addSubview(categoriesLabelAndImageView)
+        view.addSubview(categoriesCollectionView)
     }
-
-
 }
 
 extension CategoriesViewController {
@@ -25,7 +25,12 @@ extension CategoriesViewController {
             categoriesLabelAndImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
             categoriesLabelAndImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             categoriesLabelAndImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -15),
-            categoriesLabelAndImageView.heightAnchor.constraint(equalToConstant: 120)
+            categoriesLabelAndImageView.heightAnchor.constraint(equalToConstant: 100),
+            
+            categoriesCollectionView.topAnchor.constraint(equalTo: categoriesLabelAndImageView.bottomAnchor),
+            categoriesCollectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            categoriesCollectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            categoriesCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
     }
 }
---
+++ СookBook/View/CategoriesCollectionView.swift
@@ -0,0 +1,61 @@
+import UIKit
+
+class CategoriesCollectionView: UICollectionView {
+    
+    private let collectionViewFlowLayout = UICollectionViewFlowLayout()
+    private let cellId = "cellId"
+    private let cell = CategoriesCollectionViewCell()
+    
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: .zero, collectionViewLayout: collectionViewFlowLayout)
+        
+        configure()
+        setupLayout()
+        setDelegates()
+        register(CategoriesCollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupLayout() {
+        collectionViewFlowLayout.minimumLineSpacing = 10
+        collectionViewFlowLayout.minimumInteritemSpacing = 0
+    }
+    
+    private func configure() {
+        backgroundColor = .clear
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func setDelegates() {
+        delegate = self
+        dataSource = self
+    }
+}
+
+//MARK: - Extensions
+extension CategoriesCollectionView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        18
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath) as? CategoriesCollectionViewCell else { return UICollectionViewCell()
+        }
+        return cell
+    }
+}
+
+extension CategoriesCollectionView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("selected")
+    }
+}
+
+extension CategoriesCollectionView: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: collectionView.frame.width / 2,  height: collectionView.frame.height / 7)
+    }
+}
